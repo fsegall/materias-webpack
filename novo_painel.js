4ea222a4d2e47939c8b007001ac4bd1109a0e586
fsegall <segall.felipe@gmail.com>
2020-03-04 18:24:01 -0300

    Ajusta css global para usar Styled Components

--- novo_painel.js
+++ novo_painel.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { render } from 'react-dom'
 import 'babel-polyfill'
 
+// Css global styles - Styled Components
+
+import GlobalStyle from './src/styles/global'
+
 // Redux imports
 import { Provider } from 'react-redux'
 import store from './src/redux/store'
@@ -10,12 +14,13 @@ import store from './src/redux/store'
 import App from './src/components/App'
 
 // Importar estilos
-import './src/scss/app.scss'
+// import './src/scss/app.scss'
 
 const nodetoRender = document.querySelector('#app')
 
 render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>,
   nodetoRender
